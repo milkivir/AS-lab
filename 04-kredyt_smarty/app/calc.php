0f1bc8adce8482614196e94e76fad36a995c0eb7
milkivir <83859152+milkivir@users.noreply.github.com>
2022-10-29 11:35:38 +0200

    Add files via upload

---
+++ 04-kredyt_smarty/app/calc.php
@@ -0,0 +1,90 @@
+<?php
+// KONTROLER strony kalkulatora
+require_once dirname(__FILE__).'/../config.php';
+//załaduj Smarty
+require_once _ROOT_PATH.'/lib/smarty/Smarty.class.php';
+
+//pobranie parametrów
+function getParams(&$form){
+	$form['x'] = isset($_REQUEST['x']) ? $_REQUEST['x'] : null;
+	$form['z'] = isset($_REQUEST['z']) ? $_REQUEST['z'] : null;
+	$form['y'] = isset($_REQUEST['y']) ? $_REQUEST['y'] : null;
+	
+}
+
+//walidacja parametrów z przygotowaniem zmiennych dla widoku
+function validate(&$form,&$infos,&$msgs,&$hide_intro){
+
+	//sprawdzenie, czy parametry zostały przekazane - jeśli nie to zakończ walidację
+	if ( ! (isset($form['x']) && isset($form['y']) && isset($form['z']) ))	return false;	
+	
+	//parametry przekazane zatem
+	//nie pokazuj wstępu strony gdy tryb obliczeń (aby nie trzeba było przesuwać)
+	// - ta zmienna zostanie użyta w widoku aby nie wyświetlać całego bloku intro z tłem 
+	$hide_intro = true;
+
+	$infos [] = 'Przekazano parametry.';
+
+	// sprawdzenie, czy potrzebne wartości zostały przekazane
+	if ( $form['x'] == "") $msgs [] = 'Nie podano kwoty kredytu';
+	if ( $form['y'] == "") $msgs [] = 'Nie podano oprocentowania';
+	if ( $form['z'] == "") $msgs [] = 'Nie podano ilości lat spłaty';
+	
+	//nie ma sensu walidować dalej gdy brak parametrów
+	if ( count($msgs)==0 ) {
+		// sprawdzenie, czy $x i $y są liczbami całkowitymi
+		if (! is_numeric( $form['z'] )) $msgs [] = 'Ilość lat nie jest wartością całkowitą';
+	}
+	
+	if (count($msgs)>0) return false;
+	else return true;
+}
+	
+// wykonaj obliczenia
+function process(&$form,&$infos,&$msgs,&$result){
+	$infos [] = 'Parametry poprawne. Wykonuję obliczenia.';
+	
+	//konwersja parametrów na int
+	$form['x'] = floatval($form['x']);
+	$form['z'] = intval($form['z']);
+	$form['y'] = floatval($form['y']);
+	
+	//wykonanie operacji
+	
+	$result = ($form['x']*(1+($form['y']/100)))/($form['z']*12);
+	
+
+}
+
+//inicjacja zmiennych
+$form = null;
+$infos = array();
+$messages = array();
+$result = null;
+$hide_intro = false;
+	
+getParams($form);
+if ( validate($form,$infos,$messages,$hide_intro) ){
+	process($form,$infos,$messages,$result);
+}
+
+// 4. Przygotowanie danych dla szablonu
+
+$smarty = new Smarty();
+
+$smarty->assign('app_url',_APP_URL);
+$smarty->assign('root_path',_ROOT_PATH);
+$smarty->assign('page_title','-KALKULATOR KREDYTOWY-');
+$smarty->assign('page_description','Przybliżone obliczanie raty kredytu');
+$smarty->assign('page_header','Szablony Smarty');
+
+$smarty->assign('hide_intro',$hide_intro);
+
+//pozostałe zmienne niekoniecznie muszą istnieć, dlatego sprawdzamy aby nie otrzymać ostrzeżenia
+$smarty->assign('form',$form);
+$smarty->assign('result',$result);
+$smarty->assign('messages',$messages);
+$smarty->assign('infos',$infos);
+
+// 5. Wywołanie szablonu
+$smarty->display(_ROOT_PATH.'/app/calc.html');
