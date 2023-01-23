29c48c26ab66f7ef2f2eaeefaeca0e5792efc516
milkivir <83859152+milkivir@users.noreply.github.com>
2023-01-23 17:30:30 +0100

    Add files via upload

---
+++ logo/public/templates_c/07c286ca53e542237a003b1a73afb0f5e8a2bfa1_0.file.main.tpl.php
@@ -0,0 +1,64 @@
+<?php
+/* Smarty version 3.1.33, created on 2023-01-22 22:42:32
+  from 'C:\xampp\htdocs\logo\app\views\main.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_63cdadc840afb4_93007760',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '07c286ca53e542237a003b1a73afb0f5e8a2bfa1' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\logo\\app\\views\\main.tpl',
+      1 => 1674423745,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_63cdadc840afb4_93007760 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
+<!DOCTYPE HTML>
+<!--
+	Halcyonic by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Logopedia</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+	<div style="margin: 1em;">
+		<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_73463271163cdadc840aa20_53918652', 'content');
+?>
+
+	</div>
+</body>
+</html><?php }
+/* {block 'content'} */
+class Block_73463271163cdadc840aa20_53918652 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'content' => 
+  array (
+    0 => 'Block_73463271163cdadc840aa20_53918652',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+ Domyślna treść zawartości .... <?php
+}
+}
+/* {/block 'content'} */
+}
